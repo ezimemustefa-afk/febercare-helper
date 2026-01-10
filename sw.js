dece992fbf20ce025c5e7a2d49de90039dd1aa63
ezimemustefa-afk <ezime.mustefa@gmail.com>
2026-01-10 13:48:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
